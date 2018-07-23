f4b64462a7410809ee0189fbcd1c95af605d15cf
Carlos <karlossanpa@gmail.com>
2018-07-23 13:32:43 +0200

    Primera versión GUI

--- Ahorcado/src/Ahorcado/Letra.java
+++ Ahorcado/src/Ahorcado/Letra.java
@@ -1,22 +1,33 @@
 package Ahorcado;
 
 // Una letra almacena el carácter y un booleano que nos indica si la hemos adivinado.
+import static java.lang.Character.toLowerCase;
+import java.text.Collator;
+
 public class Letra {
 
     private char letra;
     private boolean acertada = false;
     
+    // Collator se usa para que los acentos no se tengan en cuenta.
+    final Collator comparador = Collator.getInstance();
+
     //Constructor
     public Letra(char l) {
-        letra = l;
+        letra = toLowerCase(l);
+        comparador.setStrength(Collator.NO_DECOMPOSITION);
     }
 
     //Devuelve si la letra coincide con el parámetro. Si es así, actualizamos el atributo.
     public boolean comprobar(char otra) {
-        if (letra == otra) {
+        // No tengo en cuenta mayúsculas y minúsculas
+        otra = toLowerCase(otra);
+        boolean iguales=comparador.compare(Character.toString(letra), Character.toString(otra))==0;
+
+        if (iguales) {
             acertada = true;
         }
-        return letra == otra;
+        return iguales;
     }
 
     //Nos dice si hemos acertado esa letra.
